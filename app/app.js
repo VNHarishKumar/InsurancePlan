04d96e70446d7982e6eefcce3f62d0244f8d8460
VNHarishKumar <72001980+VNHarishKumar@users.noreply.github.com>
2024-04-19 13:33:00 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,13 +1,11 @@
 import express from "express";
 import cors from "cors";
-// import bodyParser from "body-parser";
 import router from "./routes/index.js";
 
 
 const app =express();
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser.text());
 app.use(express.urlencoded());
 
 
